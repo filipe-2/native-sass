1ef6dc94705ba9c3db32104f7d5e7cf724480412
Filipe Ramos <146658398+filipe-2@users.noreply.github.com>
2024-11-13 23:24:17 -0300

    Refactor: Add utility functions to simplify the sassy function

--- src/utils.ts
+++ src/utils.ts
@@ -27,3 +27,30 @@ export const handleSharedStyles = (
     Object.assign(map[scopedKey], value);
   });
 };
+
+
+// Checks if a value is an object
+export const isObject = (value: any): boolean => typeof(value) === 'object' && !Array.isArray(value);
+
+
+// Assings styles directly if they're not objects
+export const assignFlatStyle = (nativeStyles: NativeStyle, parentKey: string, key: string, value: string | number) => {
+  if (!nativeStyles[parentKey]) nativeStyles[parentKey] = {};
+  nativeStyles[parentKey][key] = value;
+}
+
+
+// Assigns ignored keys without flattening
+export const assignIgnoredKeyStyle = (nativeStyles: NativeStyle, parentKey: string, key: string, value: NestedStyle) => {
+  if (!nativeStyles[parentKey]) nativeStyles[parentKey] = {};
+  nativeStyles[parentKey][key] = value;
+}
+
+
+// Applies shared styles to each relevant selector in the nativeStyles object
+export const applySharedStyles = (nativeStyles: NativeStyle, sharedStylesMap: { [key: string]: NestedStyle }) => {
+  for (const selector in sharedStylesMap) {
+    if (!nativeStyles[selector]) nativeStyles[selector] = {};
+    Object.assign(nativeStyles[selector], sharedStylesMap[selector]);
+  }
+}
