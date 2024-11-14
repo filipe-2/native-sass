0241ef8aa55b6ce7ff7f98c2ec5137bb50484e1d
Filipe Ramos <146658398+filipe-2@users.noreply.github.com>
2024-11-13 22:14:15 -0300

    refactor: Update index.ts

--- index.ts
+++ index.ts
@@ -2,15 +2,16 @@ import { NativeStyle, NestedStyle } from './src/types';
 import { capitalize, handleSharedStyles } from './src/utils';
 
 // List of keys that should not be flattened (compound styles)
-const ignoredKeys = ['shadowOffset'];
+const ignoredKeys: string[] = ['shadowOffset'];
 
 export function sassy(nestedStyles: NestedStyle, parentKey: string = ''): NativeStyle {
   const nativeStyles: NativeStyle = {};
   const sharedStylesMap: { [key: string]: NestedStyle; } = {};
-
+  // Traverse each key-value pair in the nested styles object
   for (const key in nestedStyles) {
     const value = nestedStyles[key];
-
+    
+    // Handle comma-separated keys (shared styles)
     if (key.includes(',')) {
       handleSharedStyles(key, parentKey, value, sharedStylesMap);
     } else {
